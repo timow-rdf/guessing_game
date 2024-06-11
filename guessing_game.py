08afdc6bb74daa40a3c38066c6b071adbd11217d
timow <Timo.Weber@rdf.nuernberg.de>
2024-06-11 14:38:43 +0200

    guessing game v2.0

--- guessing_game.py
+++ guessing_game.py
@@ -1,4 +1,5 @@
 # guessing game v1.0
+# guessing game v2.0
 
 import random
 
@@ -10,14 +11,21 @@ guess_correct = False
 count_attempts = 0
 
 while not guess_correct:
-    guess = int(input('Guess a number between 1 and 100: '))
+    try:
+        guess = int(input('Guess a number between 1 and 100: '))
 
-    count_attempts += 1
+        if guess < 1 or guess > 100:
+            raise ValueError
 
-    if number == guess:
-        print(f'Congratulation, your guess {guess} is correct. Score: {count_attempts}')
-        guess_correct = True
-    elif number < guess:
-        print(f'The number you are looking for is smaller than your guess {guess}!')
-    elif number > guess:
-        print(f'The number you are looking for is greater than your guess {guess}!')
+        count_attempts += 1
+
+        if number == guess:
+            print(f'Congratulation, your guess {guess} is correct. Score: {count_attempts}')
+            guess_correct = True
+        elif number < guess:
+            print(f'The number you are looking for is smaller than your guess {guess}!')
+        elif number > guess:
+            print(f'The number you are looking for is greater than your guess {guess}!')
+
+    except ValueError:
+        print('Wrong Input!')
