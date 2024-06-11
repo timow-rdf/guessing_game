253a0ce294f055631e1e15f10a338646a01ad135
timow <Timo.Weber@rdf.nuernberg.de>
2024-06-11 14:39:25 +0200

    guessing game v3.0

--- guessing_game.py
+++ guessing_game.py
@@ -1,31 +1,48 @@
 # guessing game v1.0
 # guessing game v2.0
+# guessing game v3.0
 
 import random
 
-number = random.randint(1, 100)
-print(f'DEBUG: The number is {number}')
+def play():
+    number = random.randint(1, 100)
+    print(f'DEBUG: The number is {number}')
 
-guess_correct = False
+    guess_correct = False
 
-count_attempts = 0
+    count_attempts = 0
 
-while not guess_correct:
-    try:
-        guess = int(input('Guess a number between 1 and 100: '))
+    while not guess_correct:
+        try:
+            guess = int(input('Guess a number between 1 and 100: '))
 
-        if guess < 1 or guess > 100:
-            raise ValueError
+            if guess < 1 or guess > 100:
+                raise ValueError
 
-        count_attempts += 1
+            count_attempts += 1
 
-        if number == guess:
-            print(f'Congratulation, your guess {guess} is correct. Score: {count_attempts}')
-            guess_correct = True
-        elif number < guess:
-            print(f'The number you are looking for is smaller than your guess {guess}!')
-        elif number > guess:
-            print(f'The number you are looking for is greater than your guess {guess}!')
+            if number == guess:
+                print(f'Congratulation, your guess {guess} is correct. Score: {count_attempts}')
+                guess_correct = True
+            elif number < guess:
+                print(f'The number you are looking for is smaller than your guess {guess}!')
+            elif number > guess:
+                print(f'The number you are looking for is greater than your guess {guess}!')
 
-    except ValueError:
-        print('Wrong Input!')
+        except ValueError:
+            print('Wrong Input!')
+
+while True:
+    play()
+
+    correctUserInput = False
+
+    while not correctUserInput:
+        prompt = input('Do you want to play again? [Yes/No] ')
+
+        if prompt == 'Yes':
+            correctUserInput = True
+        elif prompt == 'No':
+            exit()
+        else:
+            print('Wrong Input! Only \'Yes\' or \'No\'')
